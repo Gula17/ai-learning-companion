c9d557a5f0cb52393fa89e5f0adae8ba1421528a
Gul Mohammad <iamgula17@gmail.com>
2026-09-17 03:18:01 +0530

    Fix CORS policy error

--- backend/main.py
+++ backend/main.py
@@ -14,9 +14,10 @@ app = FastAPI()
 # Allow Frontend to communicate with Backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
 )
 
 # Global variable to store the vector database in memory
